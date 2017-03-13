8587f7cf3c596a7adfba5f9ef3286161839f5e89
PiotrLenarczyk <p.lenarczyk@ibles.waw.pl>
2017-03-13 13:04:27 +0100

    platform-independent clocks counter

--- 26_staticVar/staticVecD.cpp
+++ 26_staticVar/staticVecD.cpp
@@ -14,17 +14,17 @@ uint16_t i = 0;
 //global variables
 uint16_t vecRows = 10;
 uint16_t vecCols = 9000;
-vector < float > vec1D( vecRows, 0.1f ); //1st global variable declaration 
-vector < vector < float > > vec2D( vecCols, vec1D ); //speedup varyies depending on hardware should be not less than a ~10
+// vector < float > vec1D( vecRows, 0.1f ); //1st global variable declaration 
+// vector < vector < float > > vec2D( vecCols, vec1D ); //speedup varyies depending on hardware should be not less than a ~10
 
 int main( void )
 {
 	clock_t t( clock() );
-// 	vector < float > vec1D( vecRows, 0.1f ); //2nd local variable declaration
-// 	vector < vector < float > > vec2D( vecCols, vec1D ); //comment and try 1st
+	vector < float > vec1D( vecRows, 0.1f ); //2nd local variable declaration
+	vector < vector < float > > vec2D( vecCols, vec1D ); //comment and try 1st
 	for ( i = 0; i < 4; i++ )
 		printf( "vec[ %i ][ 1 ]: %f\n", i, vec2D[ i ][ 1 ] );
-	cout << "elapsed time: " << float( clock() - t ) / 1000 << "[ms]" << endl;
+	cout << "elapsed time: " << 1E3 * float( clock() - t ) / CLOCKS_PER_SEC << "[ms]" << endl;
 
 	return 0;
 }
