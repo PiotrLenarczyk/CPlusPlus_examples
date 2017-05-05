a207109053b5b13c8a8125dc914b643999dbbe45
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-05-05 12:31:40 +0200

    list<vector<float>>

--- 30_list/listExample.cpp
+++ 30_list/listExample.cpp
@@ -1,6 +1,7 @@
 //STL
 #include <iostream>
 #include <list>
+#include <vector>
 /*
         https://www.youtube.com/watch?v=4-u9R40G1j8
 */ 
@@ -24,5 +25,12 @@ int main( void )
     for ( dListPtr i = l.begin(); i != l.end(); i++ )
         cout << "l: " << *i << endl;
     
+    vector < float > tmpVec( 10, 1.11f );
+    list< vector< float > > lVec( 10, tmpVec );
+    list< vector< float > >::iterator lVecPtr = lVec.begin();
+    cout << "l[0][1]: " << (*lVecPtr)[ 1 ] << endl;
+    cout << "l[0][1]: " << (*lVecPtr).at( 1 ) << endl;
+
     return 0;
 }
+//P.S. theretically list< vector< float > > should be more efficient than vector < vector < float > > vecD, in my personal opinion, but I am not sure.
