7f7a7188fa4cadf26205f978ab1a2048a6bfcf7d
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-05-29 21:29:05 +0200

    static load check

--- 34_energyService/energyService.sh
+++ 34_energyService/energyService.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#also one can run: 
-#qmake && make && ./a.out
-touch a.out && rm a.out
-qmake && make -j `nproc` && make clean && clear && ./a.out 
-
-
+tresh="0.2"
+loadPC=(`cat /proc/loadavg`); echo "host loadvg: "${loadPC[2]}" (treshold=$tresh)"
+echo "=========================================================="
+echo "checking servers loads:"
+SERVERS=("localhost" "192.168.0.11")
+for SERVER in "${SERVERS[@]}"
+do
+    loadPC=(`ssh lenarczp@$SERVER 'cat /proc/loadavg'`)
+    echo "[ $SERVER ] loadavg: "${loadPC[2]}
+    if (( `echo "${loadPC[2]} < $tresh" | bc -l` == 1 ))
+    then
+        echo "[ $SERVER ] shutdown now"
+    fi
+    echo "----------------------------------------------------------"
+done
+echo "...done"
+echo "=========================================================="
