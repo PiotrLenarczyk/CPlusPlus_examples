04424d432c9cb8f0b529db3648637f1c0ce7fb0f
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-07-09 20:31:42 +0200

    README

--- 02_MINIMAL/minimal.cpp
+++ 02_MINIMAL/minimal.cpp
@@ -1,14 +1,15 @@
 //          ./a.out 1122q
+unsigned ind, i;
 int main( int argc, char* argv[] ) 
 {
     char forkNo[ 20 ] = {'0'}; int argSize = 0;//needs fork handle; example fork number = 1122
-    for ( int ind = 0; ind < 20; ind++ )
+    for ( ind = 0; ind < 20; ind++ )
         if ( argv[ argc - 1 ][ ind ] != 'q' )
             forkNo[ ind ] = argv[ argc - 1 ][ ind ];
         else
             argSize = ind;
     int vecSize = 10; float vec[ vecSize ] = { 0.21 }; //sample vector
-    for ( int i = 0; i < vecSize; i++ )
+    for ( i = 0; i < vecSize; i++ )
         vec[ i ] *= vec[ i ] + i;
     //further fork calculations...
     
