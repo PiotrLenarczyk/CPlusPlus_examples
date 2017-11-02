53036f15a06d3ce5c7080a9c4e7eb6ca83cc349f
Piotr Lenarczyk <piotrl@elprotronic.com>
2017-11-02 08:27:58 +0100

    preprocesor makro dynamic objects constructor

---
+++ 59_dynamicObjects/dynamicObjects.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#define CONCATENATE(a,b) a##b
+
+using namespace std;
+typedef uint32_t uint;
+size_t i = 0;
+uint N = 1E3;
+
+template < typename T >
+inline void funInc( vector< T > &vecIn )
+{
+	for ( auto &x:vecIn )
+		x++;
+};
+
+int main( void )
+{	
+	vector < float > vecf32_0( N, 0.0f );
+	vector < float > vecf32_1( N, 0.0f );
+	funInc( vecf32_0 );
+	funInc( vecf32_1 );
+	cout << "vecf32_0[ 1 ]: " << vecf32_0[ 1 ] << endl;
+	cout << "vecf32_1[ 1 ]: " << vecf32_1[ 1 ] << endl;
+	
+	uint vecsNo = 2;
+	for ( i = 0; i < vecsNo; i++ )
+	{
+		vector < float > CONCATENATE( vecf32__, i ) ( N, 0.0f );
+		funInc( CONCATENATE( vecf32__, i ) );
+		cout << "vecf32__" << i << "[ 1 ]: " << CONCATENATE( vecf32__, i )[ 1 ] << endl;
+	}
+	
+	return 0;
+}
