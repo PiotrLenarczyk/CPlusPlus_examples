9aa653697ad0f51640f6c20ba26b5a37800ec387
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-12-19 16:47:57 +0100

    small update of errors

--- 10_async/RUN_COMMANDS.sh
+++ 10_async/RUN_COMMANDS.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #also one can run: 
 #qmake && make && ./a.out
-rm a.out
+touch a.out && rm a.out
 qmake
 make -j `nproc`
 make clean
 clear 
 ./a.out
+touch a.out && rm a.out
--- 10_async/asynchro.cpp
+++ 10_async/asynchro.cpp
@@ -3,11 +3,21 @@
 int main()
 {
     clock_t t, t1;
-    t = clock();
     vector< float > vec( 40100, 3.1f );
-    cout << "Sum: " << parallel_sum( vec.begin(), vec.end() ) << endl;
+    double result = vec.size() * vec[ 0 ];
+    t = clock();
+    double resSeq = 0.0;
+    for ( uint i = 0; i < vec.size(); i++ )
+        resSeq += vec[ i ];
+    cout << "Sum: " << resSeq << "; err = " << result - resSeq << " ( " << ( result - resSeq ) / result << "[%] )" << endl;
     t1 = clock() - t;
     cout << "Time of sequential calculations: " << ((float)(t1))/CLOCKS_PER_SEC << "[s]" << endl;
+    
+    t = clock();
+    double resPar = parallel_sum( vec.begin(), vec.end() );
+    cout << "Sum: " << resPar << "; err = " << result - resPar << " ( " << ( result - resPar ) / result << "[%] )" << endl;
+    t1 = clock() - t;
+    cout << "Time of parallel calculations: " << ((float)(t1))/CLOCKS_PER_SEC << "[s]" << endl;
     
     return 0;
 }
