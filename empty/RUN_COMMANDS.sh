c8da5e7e425c8fbec4ed1808410cb905721ba0d1
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2018-01-13 22:10:11 +0100

    empty -pipe compilation flag

--- empty/RUN_COMMANDS.sh
+++ empty/RUN_COMMANDS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-GCC_FLAGS='-mtune=native -march=native -std=c++11 -O2'
+GCC_FLAGS='-mtune=native -march=native -std=c++11 -Ofast -pipe'
 touch a.out && rm a.out && clear && g++ empty.cpp $GCC_FLAGS -o a.out && ./a.out 
 rm a.out
