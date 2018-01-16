d715b173fa1034247ec99b1218eeeccbf476adbe
Piotr Lenarczyk <piotrl@elprotronic.com>
2018-01-16 10:00:43 +0100

    scanf cp small fix

--- 76_fileScanfFprintf/fprintf_Fscanf.cpp
+++ 76_fileScanfFprintf/fprintf_Fscanf.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
 #include <vector>
 
 using namespace std;
@@ -8,13 +9,57 @@ typedef uint32_t uint;
 int i = 0;
 int sysRet;
 const uint N = 10;
+const int cpSize = sizeof( float );
+char cpBuf[ cpSize ];
+
+void trivialStream( void );
+void binaryStream( void );
+
 
 int main( void )
+{
+	//trivialStream();
+	//binaryStream();
+	
+	return 0;
+};
+
+void binaryStream( void )
 {
 	vector< float > vec( N );
 	FILE *fileWritePtr; char fileName[] = "floatNorm.txt";
 	fileWritePtr = fopen( fileName, "w" );
-	for ( ; i < N; i++ )		//it is lossy stream ( float looses precision ) - use memcpy char stream instead
+	for ( i = 0; i < N; i++ )		//it is lossy stream ( float looses precision ) - use memcpy char stream instead
+	{
+		vec[ i ] = 0.1f + float( i );
+		memcpy( &cpBuf[ 0 ], &vec[ i ], cpSize );
+		fprintf( fileWritePtr, "data[%i]: %s\n", ( int )i, cpBuf );
+	};
+	vec.resize( 0 );
+	fclose( fileWritePtr );
+	string COM = "cat ";
+	sysRet = system( COM.append( fileName ).c_str() ); if ( sysRet !=0 ) { fprintf( stderr, "\tERROR: System Command!\n" ); return; };
+	
+	FILE *fileReadPtr = fopen( fileName, "r" );
+	vector< float > vecRead( N ); int j; int readRet = 0;
+	for ( i = 0; i < N; i++ )	//lossy stream read
+	{
+		readRet = fscanf( fileReadPtr, "data[%i]: %s\n", &j, &cpBuf[ 0 ] );
+		memcpy( &vecRead[ i ], &cpBuf, cpSize );
+	};
+	fclose( fileReadPtr );
+	
+	puts( "" );
+	for ( i = 0; i < N; i++ )
+		printf( "read from file (%s) data[%i]: %f\n", fileName, i, vecRead[ i ] );
+};
+
+void trivialStream( void )
+{
+	vector< float > vec( N );
+	FILE *fileWritePtr; char fileName[] = "floatNorm.txt";
+	fileWritePtr = fopen( fileName, "w" );
+	for ( i = 0; i < N; i++ )		//it is lossy stream ( float looses precision ) - use memcpy char stream instead
 	{
 		vec[ i ] = 0.1f + float( i );
 		fprintf( fileWritePtr, "data[%i]: %f\n", ( int )i, vec[ i ] );
@@ -22,17 +67,15 @@ int main( void )
 	vec.resize( 0 );
 	fclose( fileWritePtr );
 	string COM = "cat ";
-	sysRet = system( COM.append( fileName ).c_str() ); if ( sysRet !=0 ) { fprintf( stderr, "\tERROR: System Command!\n" ); return -1; };
+	sysRet = system( COM.append( fileName ).c_str() ); if ( sysRet !=0 ) { fprintf( stderr, "\tERROR: System Command!\n" ); return; };
 	
 	FILE *fileReadPtr = fopen( fileName, "r" );
-	vector< float > vecRead( N ); int j;
+	vector< float > vecRead( N ); int j; int readRet = 0;
 	for ( i = 0; i < N; i++ )	//lossy stream read
-		fscanf( fileReadPtr, "data[%i]: %f\n", &j, &vecRead[ i ] );
+		readRet = fscanf( fileReadPtr, "data[%i]: %f\n", &j, &vecRead[ i ] );
 	fclose( fileReadPtr );
 	
 	puts( "" );
 	for ( i = 0; i < N; i++ )
 		printf( "read from file (%s) data[%i]: %f\n", fileName, i, vecRead[ i ] );
-	
-	return 0;
 };
