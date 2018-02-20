cfad84a358d125ba1a040793d2d88a3333763dc6
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2018-02-20 20:54:56 +0100

    small fix

--- 81_throughputRAM/RUN_COMMANDS.sh
+++ 81_throughputRAM/RUN_COMMANDS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #sudo apt-get install mbw
-for ((i=512;i<4096;i+=512))	##single core accesses!
+for ((i=1;i<4096;i+=512))	##single core accesses (from 1MB up to 4GB)!
 do
-	mbw -n 1 $i # &   ##uncomment for dual core accesses
-#	mbw -n 1 $i 
+	mbw -n 4 $i -t2   # &   ##uncomment for dual core accesses
+#	mbw -n 4 $i -t2 
 done
 
 #Post Scriptum: one can see that motherboard bus width is constant, and RAM
-#memory bandwidth is divided amongs CPU cores.
+#memory bandwidth is divided amongs CPU cores. Obtain you RAM capabilities
+#details with memtestx86+ bootable USB.
