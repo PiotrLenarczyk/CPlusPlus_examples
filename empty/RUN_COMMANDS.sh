5f5afea023d50deea5b85706d0a9f218d26135b7
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2018-02-20 18:26:30 +0100

    small empty/ update

--- empty/RUN_COMMANDS.sh
+++ empty/RUN_COMMANDS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 GCC_FLAGS='-mtune=native -march=native -std=c++11 -Ofast -pipe';
-touch a.out && rm a.out && clear && g++ -o a.out $GCC_FLAGS empty.cpp && ./a.out 
+LIBS=''
+touch a.out && rm a.out && clear && g++ -o a.out $GCC_FLAGS empty.cpp $LIBS && ./a.out 
 rm a.out
