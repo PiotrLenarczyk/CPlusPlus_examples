4acacf21ad019d6acd984b7a58f52912fb64c556
Piotr Lenarczyk <piotrl@elprotronic.com>
2018-02-22 07:45:53 +0100

    default argument of function

---
+++ 82_defaultArgument/RUN_COMMANDS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+GCC_FLAGS='-mtune=native -march=native -std=c++11 -Ofast -pipe';
+LIBS=''
+touch a.out && rm a.out && clear && g++ -o a.out $GCC_FLAGS defArg.cpp $LIBS && ./a.out 
+rm a.out
--- 82_defaultArgument/defArg.cpp
+++ 82_defaultArgument/defArg.cpp
@@ -4,14 +4,30 @@ typedef uint32_t uint;
 uint i = 0;
 
 __attribute__((always_inline)) static inline 
-void foo( void )
-{
+void foo( const int in = 1, int* out = nullptr )
+{	printf( "in = %i\n", in );
+	if ( out != nullptr )
+		printf( "*out : %i\n", *out );
+};
+
+__attribute__((always_inline)) static inline 
+void bar( const uint *in = nullptr, const uint inSize = 0 )
+{	for ( i = 0; i < inSize; i++ )
+		printf( "in[%i] : %u\n", i, in[ i ] );
 };
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 int main( void )
-{	foo();
+{	int a = 5;
+	foo( -1 );
+	foo( a );
+	foo( a, &a );
+	
+#define N 3
+	uint arr[ N ] = { 5, 6, 11 };
+	bar( &arr[ 0 ], N );
+	bar();	//it is not an error!
 	
 	return 0;
 };//end of main()
