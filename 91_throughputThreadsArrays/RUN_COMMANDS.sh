119c9b872c54e8db88b8143ea1e1dc0217949ba6
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2018-03-26 15:35:57 +0200

    small update

--- 91_throughputThreadsArrays/RUN_COMMANDS.sh
+++ 91_throughputThreadsArrays/RUN_COMMANDS.sh
@@ -3,5 +3,5 @@ GCC_OPT='-mtune=native -march=native -std=c++11 -pipe';
 GCC_FLAGS='-fmax-errors=3 -fpermissive'
 LIBS='-pthread'
 touch a.out && rm a.out && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS thArr.cpp $LIBS && for ((i=0;i<3;i++)); do ./a.out;done;
-sudo perf stat -B -e cache-references,cache-misses ./a.out
+sudo perf stat -B -e cache-references,cache-misses -r 5 ./a.out
 rm a.out
