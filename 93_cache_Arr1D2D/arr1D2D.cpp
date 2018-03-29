779d45c63e9d246fa6e8b4316024a2799db2416d
Piotr Lenarczyk <piotrl@elprotronic.com>
2018-03-29 10:38:47 +0200

    small update

--- 93_cache_Arr1D2D/arr1D2D.cpp
+++ 93_cache_Arr1D2D/arr1D2D.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+typedef long unsigned uint64;
 long unsigned i, sizeGB = 1, N = sizeGB*1024lu*1024lu*1024lu;
 unsigned t, y, x;
 const unsigned Y = 8, X = N / Y;
 const unsigned X2 = 8, Y2 = N / Y;
 
 int main( void )
-{   t = clock();
+{   
 ////	X = N; it is size-optimal
     //unsigned char* arr = ( unsigned char* )malloc( N ); 
+		//t = clock();
         //i = 0; while( i < N ) //1GiB array
         //{   arr[ i ] += arr[ i ];
             //i++;
@@ -20,33 +22,49 @@ int main( void )
                      //float( sizeGB ) / ( float( t ) / float( CLOCKS_PER_SEC ) ), t );
     //delete( arr );
     
-////	YX access; Y = 8; it is size less optimal, but better use of cache
-    unsigned char** arr2D = ( unsigned char** )malloc( Y * sizeof( unsigned char* ) );
-    i=0;while( i < Y )
-    {   arr2D[ i ] = ( unsigned char* )malloc( X );
-		i++;
-	};
-		t = clock();
-        i = 0; while( i < X ) //1GiB array in a 8B pieces
-        {   arr2D[ 0 ][ i ] += arr2D[ 0 ][ i ];
-			arr2D[ 1 ][ i ] += arr2D[ 1 ][ i ];
-			arr2D[ 2 ][ i ] += arr2D[ 2 ][ i ];
-			arr2D[ 3 ][ i ] += arr2D[ 3 ][ i ];
-			arr2D[ 4 ][ i ] += arr2D[ 4 ][ i ];
-			arr2D[ 5 ][ i ] += arr2D[ 5 ][ i ];
-			arr2D[ 6 ][ i ] += arr2D[ 6 ][ i ];
-			arr2D[ 7 ][ i ] += arr2D[ 7 ][ i ];
-            i += Y;
-        };
-        t = clock() - t;
-        if ( t < 500 ) printf( "\tERROR!\n" );
-        else printf( "throughput arr2D YX: %02f[GBps]; elapsed: %u[periods]\n",  
-                     float( sizeGB ) / ( float( t ) / float( CLOCKS_PER_SEC ) ), t );
-    i=0;while( i < Y )
-    {   delete( arr2D[ i ] );
-		i++;
-	};
-    delete( arr2D );
+////	64b memory accesses
+    //unsigned char* arr = ( unsigned char* )malloc( N ); 
+    //uint64 dummy64;
+		//t = clock();
+        //i = 0; while( i < N ) //1GiB array
+        //{   dummy64 = *( uint64* )&arr[ i ];
+			//dummy64 += dummy64;
+			//*( uint64* )&arr[ i ] = dummy64;
+            //i += 8;
+        //};
+        //t = clock() - t;
+        //if ( t < 500 ) printf( "\tERROR!\n" );
+        //else printf( "throughput arr1D: %02f[GBps]; elapsed: %u[periods]\n",  
+                     //float( sizeGB ) / ( float( t ) / float( CLOCKS_PER_SEC ) ), t );
+    //delete( arr );
+   
+//////	YX access; Y = 8; it is size less optimal, but better use of cache
+    //unsigned char** arr2D = ( unsigned char** )malloc( Y * sizeof( unsigned char* ) );
+    //i=0;while( i < Y )
+    //{   arr2D[ i ] = ( unsigned char* )malloc( X );
+		//i++;
+	//};
+		//t = clock();
+        //i = 0; while( i < X ) //1GiB array in a 8B pieces
+        //{   arr2D[ 0 ][ i ] += arr2D[ 0 ][ i ];
+			//arr2D[ 1 ][ i ] += arr2D[ 1 ][ i ];
+			//arr2D[ 2 ][ i ] += arr2D[ 2 ][ i ];
+			//arr2D[ 3 ][ i ] += arr2D[ 3 ][ i ];
+			//arr2D[ 4 ][ i ] += arr2D[ 4 ][ i ];
+			//arr2D[ 5 ][ i ] += arr2D[ 5 ][ i ];
+			//arr2D[ 6 ][ i ] += arr2D[ 6 ][ i ];
+			//arr2D[ 7 ][ i ] += arr2D[ 7 ][ i ];
+            //i += Y;
+        //};
+        //t = clock() - t;
+        //if ( t < 500 ) printf( "\tERROR!\n" );
+        //else printf( "throughput arr2D YX: %02f[GBps]; elapsed: %u[periods]\n",  
+                     //float( sizeGB ) / ( float( t ) / float( CLOCKS_PER_SEC ) ), t );
+    //i=0;while( i < Y )
+    //{   delete( arr2D[ i ] );
+		//i++;
+	//};
+    //delete( arr2D );
     
 ////	XY access; X2 = 8; //it is not used RAM size optimal - worse information density, than YX access
     //unsigned char** arr2DXY = ( unsigned char** )malloc( Y2 * sizeof( unsigned char* ) );
