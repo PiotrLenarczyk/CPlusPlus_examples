dfcbf2fb0b52acbf396e570e4763394ce876a47c
Piotr Lenarczyk <piotrl@elprotronic.com>
2018-05-11 14:05:57 +0200

    small update

--- 99_appendC/appendC.cpp
+++ 99_appendC/appendC.cpp
@@ -1,64 +1,98 @@
-#include <stdio.h> 
-#include <cstring>
+#include <stdlib.h>		//malloc()
+#include <stdio.h> 		//printf()
+#include <string.h>		//strlen()
 #include <string>		//not needed - just for example
-#include <stdlib.h>
 using namespace std;
 typedef unsigned char byte;
 typedef __uint32_t uint;
 uint i = 0x0;
 	
 typedef struct 
-{	char* dataPtr;
-	char* endPtr;
-	uint strSize;
+{	char* dataPtr = NULL;
+	char* endPtr = NULL;
+	uint strSize = 0x0;
+//======================================================================
 	void mallocStr( uint size )
 	{	dataPtr = ( char* )malloc( size );
 		dataPtr[ size - 1 ] = '\0';
 		endPtr = dataPtr; 
 		strSize = size;
 	};
+//======================================================================
 	uint size( void )
-	{	return endPtr - dataPtr;
+	{	return uint( endPtr - dataPtr );
 	};
-	void rellocStr( uint size )	//costly background hardcopy!
-	{	//malloc
-		//copy + update(dataPtr, endPtr, strSize)
-		//freeOld
-		return;
+//======================================================================
+	void resizeStr( uint newSize )	//costly background hardcopy! only enlarging
+	{	uint i = 0x0, currSize = size();
+		if ( newSize < currSize )
+		{	printf( "====\n\tERROR : lossless resize!\n====\n" );
+			fatalStr();
+			return;
+		};
+		char* newDataPtr = ( char* )malloc( newSize );
+		while ( i < currSize )
+		{	newDataPtr[ i ] = dataPtr[ i ];
+			i+=1;
+		}; 
+		free( dataPtr );
+		dataPtr = newDataPtr; newDataPtr = NULL;
+		dataPtr[ newSize - 1 ] = '\0';
+		endPtr = dataPtr + currSize;
+		strSize = newSize;
 	};
+//======================================================================
 	void prepend( const void* const strIn ) //costly background hardcopy!
-	{	//malloc
-		//prependCopy + update(dataPtr, endPtr, strSize)
-		//freeOld
-		return;
+	{	char* strInPtr = ( char* )strIn; 
+		uint sizeIn = strlen( strInPtr ), currSize = size(), 
+			 newSize = currSize + sizeIn, i=0x0;
+		char* newDataPtr = ( char* )malloc( newSize + 1 ); //'\0'
+		i=0x0;while ( i < sizeIn )	//prepend part
+		{	newDataPtr[ i ] = strInPtr[ i ];
+			i+=1;
+		}; strInPtr = NULL;
+		i=sizeIn;while ( i < newSize )	//hardcopy
+		{	newDataPtr[ i ] = dataPtr[ i - sizeIn ];
+			i+=1;
+		}; 
+		free( dataPtr );
+		dataPtr = newDataPtr; newDataPtr = NULL;
+		dataPtr[ newSize + 0 ] = '\0'; //malloc newDataPtr
+		endPtr = dataPtr + newSize;
+		strSize = newSize;
 	};
-	
-	
-	
+//======================================================================
 	void append( const void* const strIn )
 	{	char* strInPtr = ( char* )strIn; 
 		uint sizeIn = strlen( strInPtr ), currSize = size(), i=0x0;
 		if ( currSize + sizeIn < strSize - 1 )
-		{	while( i < sizeIn )
+		{	i=0x0;while( i < sizeIn )
 			{	dataPtr[ currSize + i ] = strInPtr[ i ];
 				i+=1;
 			}; endPtr += sizeIn;
 		}else
-		{	printf( "\tERROR : STRING full - provided costly hardcopy!\n" );
-			
-			//rellocStr()
-			//copy
-			
+		{	uint newSize = currSize + sizeIn;
+			resizeStr( newSize );
+			i=0x0;while( i < sizeIn )
+			{	dataPtr[ currSize + i ] = strInPtr[ i ];
+				i+=1;
+			}; endPtr += sizeIn;
+			printf( "====\n\tERROR : STRING full - provided costly hardcopy!\n====\n" );		
 		};
+		strInPtr = NULL;
 	};
+//======================================================================
 	void freeStr( void )
-	{	free( dataPtr );
+	{	free( dataPtr ); dataPtr = NULL;
 		strSize = 0x0;
-		dataPtr = NULL;
 		endPtr = NULL;
 	};
+//======================================================================
+	void fatalStr( void )
+	{	printf( "====\n\tERROR : Program corruption! STRING_OBJECT.free();\n====\n" );
+		freeStr();
+	};
 } STRING;
-
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -66,15 +100,23 @@ int main( void )
 {	char buf[] = "SomeText";
 	const char* buff2 = " other!";
 	string theString = " C++11";
-	STRING str;str.mallocStr( 0x100 );
+	STRING str;str.mallocStr( 0x20 );
 		str.append( "aa" ); 
 		str.append( buf );
 		str.append( buff2 );
 		str.append( theString.c_str() );
 		str.append( " array." );
 		printf( "[%s].size() : %u\n", str.dataPtr, str.size() );
+		printf( "&str : %p\n", str.dataPtr );
+		str.resizeStr( 0x30 );
+		printf( "[%s].size() : %u\n", str.dataPtr, str.size() );
+		printf( "&str : %p\n", str.dataPtr );
+		str.prepend( "prependPattern_" );
+		printf( "[%s].size() : %u\n", str.dataPtr, str.size() );
+		str.append( "_StringOverflow" );
+		printf( "[%s].size() : %u\n", str.dataPtr, str.size() );
+		//str.resizeStr( 0x10 ); //lossy resize
 	str.freeStr();
-
 	
 	return 0;
 };//end of main()
