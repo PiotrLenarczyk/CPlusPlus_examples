c877c38f29b22703d0c12f7c8564965921ac28bb
Piotr Lenarczyk <piotrl@elprotronic.com>
2018-05-24 14:10:07 +0200

    small update

--- 102_time/time.cpp
+++ 102_time/time.cpp
@@ -10,7 +10,7 @@ typedef __uint32_t uint;
 uint i = 0x0;
 
 #define CLOCKS_PER_SEC  ( __uint64_t )1000000
-extern __inline__ unsigned long long time( void )
+extern __inline__ __uint64_t time( void )
 {   uint hi, lo;
     __asm__ __volatile__ ( "rdtsc" : "=a"( lo ), "=d"( hi ) );
     return ( ( __uint64_t )lo ) | ( ( ( __uint64_t )hi ) << 32 );
