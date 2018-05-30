6d26bd8040451d59819c645db3e7a67850ce98d3
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2018-05-30 20:01:36 +0200

    small update

--- 105_binCmp/cmp.cpp
+++ 105_binCmp/cmp.cpp
@@ -5,7 +5,6 @@ int main( int argc, char* argv[] )
 	inB = ( unsigned char* )&argv[ 2 ][ 0 ];
 	while( *++inA != *++inB )
 		;
-	if ( *inA == '\0' ) return 0; //array end
 	--inA;--inB;
 	if ( *inA < *inB ) return -1;
 	if ( *inA > *inB ) return 2;
