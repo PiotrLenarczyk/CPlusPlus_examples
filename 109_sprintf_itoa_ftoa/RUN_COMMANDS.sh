52cea7615b63c9f2e50c26db8527dce80e7de4c0
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2018-10-04 22:28:01 +0200

    small update

---
+++ 109_sprintf_itoa_ftoa/RUN_COMMANDS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GCC_OPT='-mtune=native -march=native -std=c++11 -Ofast -pipe'
+GCC_FLAGS='-fmax-errors=3 -Wno-write-strings -g'
+LIBS=''
+touch a.out && rm a.out && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS sprintfItoa.cpp $LIBS && 
+gdb ./a.out -ex "tui enable" -ex "set pagination off"  -ex "set output-radix 16" -ex "tui enable" -ex "set confirm off" -ex "b 137" -ex "r"	&&
+rm a.out	&&
+exit 0;
