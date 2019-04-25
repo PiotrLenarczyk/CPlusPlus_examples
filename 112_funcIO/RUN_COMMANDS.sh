6dd69b87ce841696cfb20afceb876e8a25e5d3bc
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2019-04-25 19:54:36 +0200

    small update

--- 112_funcIO/RUN_COMMANDS.sh
+++ 112_funcIO/RUN_COMMANDS.sh
@@ -3,5 +3,5 @@ GCC_OPT='-mtune=native -march=native -std=c++11 -Ofast -pipe'
 GCC_FLOAT='-Wdouble-promotion -fsingle-precision-constant'
 GCC_FLAGS='-fmax-errors=3'
 LIBS=''
-touch a.out && rm a.out && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS $GCC_FLOAT empty.cpp $LIBS && ./a.out
+touch a.out && rm a.out && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS $GCC_FLOAT funcIO.cpp $LIBS && ./a.out
 rm a.out
--- 112_funcIO/funcIO.cpp
+++ 112_funcIO/funcIO.cpp
@@ -27,8 +27,8 @@ typedef struct
 	uint 	size;
 	void* 	data;	//any object
 }IO;
-IO IN 		= { 0x0, NULL };
-IO OUT 		= { 0x0, NULL };
+IO IN 		= { 0x10, ( char* )"in data" };
+IO OUT 		= { 0x10, ( char* )"out data" };
 IO EMPTY 	= { 0x0, NULL };
 
 //function struct
@@ -54,19 +54,30 @@ void FOO( IO* out, IO* in )
 		printf( "output: \n\tsize : 0x%X \n\tdata: [%s]\n", out->size, (char*)out->data );
 };//FOO()
 
+FUNCTION allFuncs[] = 
+{	{ "first", SEND, 0x0, &EMPTY, &EMPTY, &FOO },
+	{ "second", SEND, 0x0, &EMPTY, &EMPTY, &FOO },
+	{ 0x0 }
+};
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 int main( void )
 {	//local data exchange
 	IO someInput = { 0x10, ( void* )"Hello World!" };
 	IO someOutput = { 0x0, NULL };
-	FUNCTION foo = { "foo", SEND, NULL, &EMPTY, &someInput, &FOO };
+	FUNCTION foo = { "foo", SEND, 0x0, &EMPTY, &someInput, &FOO };
 	foo.run();
 	
 	//global data exchange
 	OUT = { 0x20, ( void* )"sample output from func!" };
 	FUNCTION bar = { "bar", RECEIVE, 0, &OUT, &EMPTY, &FOO };
 	bar.run();
+	
+	i = 0x0; while( allFuncs[ i ].type )
+	{	allFuncs[ i ].run();
+		i++;
+	};
 	return 0;
 };//end of main()
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
