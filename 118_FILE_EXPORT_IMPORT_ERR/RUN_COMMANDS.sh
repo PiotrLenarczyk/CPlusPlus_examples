fbac634247eec864cd60936715752716ff51c8ea
Piotr Lenarczyk <piotrl@elprotronic.com>
2019-09-18 11:06:09 +0200

    update

--- 118_FILE_EXPORT_IMPORT_ERR/RUN_COMMANDS.sh
+++ 118_FILE_EXPORT_IMPORT_ERR/RUN_COMMANDS.sh
@@ -3,6 +3,7 @@ GCC_OPT='-mtune=native -march=native -std=c++11 -Ofast -pipe'
 GCC_FLOAT='-Wdouble-promotion -fsingle-precision-constant'
 GCC_FLAGS='-fmax-errors=3'
 LIBS=''
-touch a.out && rm -f a.out data.bin && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS $GCC_FLOAT importExport.cpp $LIBS && ./a.out && 
+PROG_FLAGS='-DSELF_CONTAINED'
+touch a.out && rm -f a.out data.bin && clear && g++ -o a.out $GCC_OPT $GCC_FLAGS $GCC_FLOAT $PROG_FLAGS importExport.cpp $LIBS && ./a.out && 
 hexdump -Cv -n 32 data.bin
 rm a.out 
