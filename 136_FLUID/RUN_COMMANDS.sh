8778b821f51fa59bcfb558610b54eefdc841ccc3
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2021-03-10 09:50:45 +0100

    small update

--- 136_FLUID/RUN_COMMANDS.sh
+++ 136_FLUID/RUN_COMMANDS.sh
@@ -13,4 +13,32 @@ then
 	cd ..
 fi
 
-fltk/fluid/fluid
+
+PROG=00_hello
+
+fltk/fluid/fluid -c ${FILE%.*}.fl
+FILE=${PROG}.cxx
+compile()
+{	rm -f ${1%.*} ${1%.*}.o && clear							&&
+	make ${1%.*}												&&
+	./${1%.*}													&&
+	rm -f ${1%.*} ${1%.*}.o
+};
+
+compile_loop()
+{	old_CS=0
+	new_CS=( $( md5sum $1 ) )
+	while [ 0 -lt 1 ]
+	do
+		if [ $old_CS[0] != $new_CS[0] ]
+		then
+			old_CS=$new_CS										&&
+#			clear												&&
+			compile $1											
+		fi
+		sleep 0.1
+		new_CS=( $( md5sum $1 ) )
+	done
+};
+
+compile_loop $FILE
